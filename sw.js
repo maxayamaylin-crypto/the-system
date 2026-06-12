2fe54feede5a910d47575befb6e8d0d8c3785566
maxayamaylin-crypto <maxayamaylin-crypto@users.noreply.github.com>
2026-06-12 20:24:22 +0100

    Add full training interaction pack

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "system-v4-4-train-profile";
+const CACHE = "system-v4-5-train-pack";
 const FILES = [
   "./",
   "./the-system.html",
@@ -13,7 +13,17 @@ const FILES = [
   "./assets/screens/nutrition-diary.jpg",
   "./assets/screens/nutrition-progress.jpg",
   "./assets/screens/training-workout.jpg",
-  "./assets/screens/profile-command.jpg"
+  "./assets/screens/profile-command.jpg",
+  "./assets/screens/train-t01-main.jpg",
+  "./assets/screens/train-t02-split-selector.jpg",
+  "./assets/screens/train-t03-upper-lower.jpg",
+  "./assets/screens/train-t04-ppl.jpg",
+  "./assets/screens/train-t05-arnold.jpg",
+  "./assets/screens/train-t06-full-body.jpg",
+  "./assets/screens/train-t07-custom-builder.jpg",
+  "./assets/screens/train-t08-edit-section.jpg",
+  "./assets/screens/train-t09-edit-exercise.jpg",
+  "./assets/screens/train-t10-session-complete.jpg"
 ];
 self.addEventListener("install", e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)).then(() => self.skipWaiting()));
